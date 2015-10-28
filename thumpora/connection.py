880eb78fd9500d56eac40e8d5f283d7868f646be
micha <michael@rudolph.io>
2015-10-28 21:57:12 +0100

    errors

--- thumpora/connection.py
+++ thumpora/connection.py
@@ -1,3 +1,4 @@
+import logging
 from botornado.s3.connection import AsyncS3Connection
 from boto.s3.connection import S3Connection
 
@@ -8,12 +9,14 @@ def get_asyncS3connection(context):
     conn = async_s3_connection
     if conn is None:
         if context.config.get('AWS_ROLE_BASED_CONNECTION', default=False):
-            conn = AsyncS3Connection()
+            conn = AsyncS3Connection("KEY", "SECRET")
+            logging.info("connection without config")
         else:
             conn = AsyncS3Connection(
                 context.config.get('AWS_ACCESS_KEY'),
                 context.config.get('AWS_SECRET_KEY')
             )
+            logging.info("connection with config: " + context.config.get('AWS_ACCESS_KEY'))
     return conn
 
 def getS3connection(context):
@@ -21,9 +24,11 @@ def getS3connection(context):
     if conn is None:
         if context.config.get('AWS_ROLE_BASED_CONNECTION', default=False):
             conn = S3Connection()
+            logging.info("connection without config")
         else:
             conn = S3Connection(
                 context.config.get('AWS_ACCESS_KEY'),
                 context.config.get('AWS_SECRET_KEY')
             )
+            logging.info("connection with config: " + context.config.get('AWS_ACCESS_KEY'))
     return conn
